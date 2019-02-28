5c0c7a0acdf149e0aaf161c416eeaf4c30286e44
huqing1473 <huqing1473@emao.com>
2019-02-28 14:12:45 +0800

    :tada: Initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import './plugins/axios'
+import App from './App.vue'
+import './registerServiceWorker'
+import './plugins/element.js'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')

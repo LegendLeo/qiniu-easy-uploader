9b54688c86a5e8ac17e9f75cd878ae85bcdb65e8
Leo <hqyx45@qq.com>
2019-03-04 22:11:16 +0800

    :wrench: 更改生产环境根目录

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const config = require('./server/config.json')
 const host = config.Host || 'http://localhost'
 
 module.exports = {
+  publicPath: '/qiniu',
   devServer: {
     proxy: host + ':' + config.Port
   }

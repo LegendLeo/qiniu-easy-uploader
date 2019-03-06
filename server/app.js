367409518d9293baf3f5b92155375c15b07dc29a
Leo <hqyx45@qq.com>
2019-03-06 22:54:58 +0800

    :sparkles: 服务端编写2中生成秘钥的方法

--- server/app.js
+++ server/app.js
@@ -1,33 +1,84 @@
 const qiniu = require('qiniu')
 const express = require('express')
 const app = express()
-const config = require('./config.json')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const path = require('path')
+const dir = path.join(__dirname, '/config.json')
+let config, option
+let PORT = 9000
 
-const mac = new qiniu.auth.digest.Mac(config.AccessKey, config.SecretKey)
-const options = {
-  scope: config.Bucket,
-  deleteAfterDays: 1,
-  returnBody:
-    `{"key":"$(key)","hash":"$(etag)","size":$(fsize),"bucket":"$(bucket)","url":"${config.Domain}/$(key)"}`
+// 为了接收post传参需要用bodyParser
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// 判断是否有config.json配置
+fs.access(dir, function (err) {
+  if (!err) {
+    config = require('./config.json')
+    if (config.port) {
+      PORT = config.port
+    }
+    option = getOption(config)
+  }
+})
+
+/**
+ * 获取生成秘钥的选项
+ * @param {Object} config 七牛云配置（包含accessKey、secretKey、domain、bucket选项）
+ */
+function getOption (config) {
+  const mac = new qiniu.auth.digest.Mac(config.accessKey, config.secretKey)
+  const options = {
+    scope: config.bucket,
+    deleteAfterDays: 1,
+    returnBody:
+      `{"key":"$(key)","hash":"$(etag)","size":$(fsize),"bucket":"$(bucket)","url":"${config.domain}/$(key)"}`
+  }
+  const putPolicy = new qiniu.rs.PutPolicy(options)
+  return {
+    mac,
+    putPolicy
+  }
 }
 
-var putPolicy = new qiniu.rs.PutPolicy(options)
+// 获取配置状态（是否在服务端配置过）
+app.get('/api/getconfig', function (req, res) {
+  res.json({
+    hasConfig: Boolean(config)
+  })
+})
 
-app.get('/api/uptoken', function (req, res, next) {
-  var token = putPolicy.uploadToken(mac)
+// 获取token（客户端传七牛云配置参数）
+app.post('/api/posttoken', function (req, res) {
+  let newConfig = req.body
+  let newOption = getOption(req.body)
+  const token = newOption.putPolicy.uploadToken(newOption.mac)
   res.header('Cache-Control', 'max-age=0, private, must-revalidate')
   res.header('Pragma', 'no-cache')
   res.header('Expires', 0)
   if (token) {
     res.json({
       uptoken: token,
-      domain: config.Domain
+      domain: newConfig.domain
     })
   }
 })
 
-app.listen(config.Port, function () {
-  console.log('Listening on port %d\n', config.Port)
-  console.log('▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽ ▽')
-  console.log('△ △ △ △ △ △ △ △ △ △ △ △ △ △ △ △ △ △ △ △ △ △ △ △ △ △ △ △ △ △\n')
+// 获取token（服务端有配置项的时候会请求此路径）
+app.get('/api/gettoken', function (req, res) {
+  const token = option.putPolicy.uploadToken(option.mac)
+  res.header('Cache-Control', 'max-age=0, private, must-revalidate')
+  res.header('Pragma', 'no-cache')
+  res.header('Expires', 0)
+  if (token) {
+    res.json({
+      uptoken: token,
+      domain: config.domain
+    })
+  }
+})
+
+app.listen(PORT, function () {
+  console.log('服务已在' + PORT + '端口启动')
 })

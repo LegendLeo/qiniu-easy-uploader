577818698c268107b1f63603831fc73de52d7cac
Leo <hqyx45@qq.com>
2019-03-07 23:34:35 +0800

    :zap: 没有用到vuex，先移除

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
 import VueClipboard from 'vue-clipboard2'
 import './registerServiceWorker'
 import './plugins/axios'
@@ -10,6 +9,5 @@ Vue.config.productionTip = false
 Vue.use(VueClipboard) // 剪贴板
 
 new Vue({
-  store,
   render: h => h(App)
 }).$mount('#app')
